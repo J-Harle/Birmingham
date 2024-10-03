d9487f095f176f8a9c42054958471602139c0777
J-Harle <158578157+J-Harle@users.noreply.github.com>
2024-10-03 16:12:31 +0100

    Update Kpoint.py

--- Kpoint.py
+++ Kpoint.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-with open("petn.cell", "r") as template_file:
-    body = template_file.readlines()
+with open("petn.cell", "r") as text:
+    body = text.readlines()
 
-for i in np.arange(0.02, 0.80, 0.01):
-    OutputFile = f"{i:.2f}petn.cell
+for i in np.arange(0.02, 0.81, 0.01):
+    OutputFile = f"{i:.2f}petn.cell" 
     with open(OutputFile, "w") as outputfile:
         outputfile.writelines(body)
-        outputfile.write(f"kpoint_mp_spacing : {i:.2f}\n") 
+        outputfile.write(f"\nkpoint_mp_spacing : {i:.2f}")
