20c809289d1ae2f7b976e352e6a3c473503cc833
J-Harle <158578157+J-Harle@users.noreply.github.com>
2024-10-04 09:13:38 +0100

    Add files via upload

---
+++ CastepBatch.py
@@ -0,0 +1,43 @@
+import os
+import subprocess
+import time
+
+# Reads current directory and finds all .cell files
+cell_files = [f for f in os.listdir('.') if f.endswith('.cell')]
+
+for cell_file in cell_files:
+    jobname = cell_file[:-5]
+    print(f"Processing file: {cell_file}")
+
+    for i in range(2,81):
+        value = f"{i:03d}"
+
+    # Simulating a .bash file for each submission
+    batch_script = f"""#!/bin/bash
+#SBATCH --time=2:00:00
+#SBATCH --mail-user=jxh1695@student.bham.ac.uk
+#SBATCH --ntasks=25
+#SBATCH --nodes=1-10
+#SBATCH --qos=bbdefault
+#SBATCH --account=michaaal-jharl
+#SBATCH --job-name={jobname}
+#SBATCH --mem=50gb
+
+set -e
+ 
+module purge; module load bluebear
+module load bear-apps/2023a
+module load CASTEP/24.1-foss-2023a
+ 
+mpirun -np $SLURM_NTASKS castep.mpi {value}petn
+
+ 
+exit
+"""
+
+    # Submit the job to SLURM
+    process = subprocess.Popen(['sbatch'], stdin=subprocess.PIPE)
+    process.communicate(input=batch_script.encode())
+
+    print(f"Submitted job for {cell_file}")
+    time.sleep(0.2)
