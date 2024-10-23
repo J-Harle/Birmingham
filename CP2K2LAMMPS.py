7398f5f82a2c3cf8f2451466fd70984c97a8ae6b
J-Harle <158578157+J-Harle@users.noreply.github.com>
2024-10-23 15:31:32 +0100

    Update CP2K2LAMMPS.py

--- CP2K2LAMMPS.py
+++ CP2K2LAMMPS.py
@@ -7,28 +7,29 @@ import numpy as np
 #import matplotlib.pyplot as plt
 
 # Open .cif file and read Coordinates and Cell Angles and Lengths
+coordinates = []
 with open("cif.cif","r") as cif_file:
     #Atom Coordinates:
-    for coordinates in line:
-        coordinates = []
+    for coordinates in cif_file:
+        if re.match(r"\b([A-Z][a-z]?)\s+[\d.-]+\s+([\d.]+)\s+([\d.]+)\s+([\d.]+", line):
         coordinates.append(line)
         
     #Angles and Side Lengths:
-    A_length = re.search(r"_cell_length_a",r)
-    B_length = re.search(r"_cell_length_b",r)
-    C_length = re.search(r"_cell_length_c",r)
-    alpha_angle = re.search(r"_cell_angle_alpha",r)
-    beta_angle = re.search(r"_cell_angle_beta",r)
-    gamma_angle = re.search(r"_cell_angle_gamma",r)
+    A_length = re.search(r"_cell_length_a\s+(\d+\.?\d*)",r)
+    B_length = re.search(r"_cell_length_b\s+(\d+\.?\d*)",r)
+    C_length = re.search(r"_cell_length_c\s+(\d+\.?\d*)",r)
+    alpha_angle = re.search(r"_cell_angle_alpha\s+(\d+\.?\d*)",r)
+    beta_angle = re.search(r"_cell_angle_beta\s+(\d+\.?\d*)",r)
+    gamma_angle = re.search(r"_cell_angle_gamma\s+(\d+\.?\d*)",r)
 
 # Create new .inp file for CP2K: 
 
-w = inp_file.writelines
+w = inp_file.write
 basis_set = BASIS_SET DZVP-MOLOPT-SR-GTH
 
 with open("new.inp","w") as inp_file:
     w("&GLOBAL")
-    w(f" PROJECT " + {filename})
+    w(f" PROJECT {filename}")
     w(" RUN_TYPE MD") # Hard coded to be be MD, but maybe this will evolve into a script that can generate any type of CP2K .inp file?
     w(" PRINT_LEVEL")
     w("&END GLOBAL")
@@ -52,32 +53,32 @@ with open("new.inp","w") as inp_file:
     w("\n")
     w(" &SUBSYS")
     w("     &CELL")
-    w(f"            "{A_length})
-    w(f"            "{B_length})
-    w(f"            "{C_length})
+    w(f"            {A_length}")
+    w(f"            {B_length}")
+    w(f"            {C_length}")
     w("     &END CELL")
     w("     &COORD")
-    w(f"            "{coordinates})
+    w(f"            {coordinates}")
     w("     &END COORD")
   
     #  BASIS SETS AND POTENTIALS FOR HYDROGEN, OXYGEN, CARBON AND NITROGEN. POTENTIALS ARE TAKEN FROM CP2K
   
     w("     &KIND H") 
-    w(f"              "{basis_set})
+    w(f"              {basis_set}")
     w("             POTENTIAL GTH-PBE-q1") 
     w("     &END KIND")
     w("\n")
     w("     &KIND O")
-    w(f"             "{basis_set}) 
+    w(f"             {basis_set}") 
     w("             POTENTIAL GTH-PBE-q6")
     w("     &END KIND")
     w("     &KIND C") 
-    w(f"              "{basis_set})
+    w(f"              {basis_set}")
     w("             POTENTIAL GTH-PBE-q4")
     w("     &END KIND")
     w("\n")
     w("     &KIND N")
-    w(f"             "{basis_set})
+    w(f"             {basis_set}")
     w("             POTENTIAL GTH-PBE-q5")
     w("     &END KIND")
     w("\n")
@@ -103,7 +104,7 @@ with open("new.inp","w") as inp_file:
     w(" &PRINT")
     w("     &TRAJECTORY")
     w("         FORMAT XYZ")
-    w(f"         FILENAME" {filename}+ ".traj")
+    w(f"         FILENAME {filename}.traj")
     w("         STRIDE 5")
     w("     &END TRAJECTORY")
     w("\n")
